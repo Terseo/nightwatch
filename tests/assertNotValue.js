a1eb78763c04a7cb187504da7d52461b30c1fc21
Terseo <44824248+Terseo@users.noreply.github.com>
2022-06-28 10:34:25 -0400

    Update assertNotValue.js

--- tests/assertNotValue.js
+++
@@ -1,7 +0,0 @@
-this.demoTest = function (browser) {
-    browser
-        .url('https://www.unosquare.com/')
-        .click("li a[href = '/ContactUs']")
-        .setValue('input[name="name"]', "Test UnosQuare")
-        .assert.not.value('input[name="name"]', "This Text Not Should be Match");
-};
