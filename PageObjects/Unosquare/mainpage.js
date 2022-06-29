b144b26784f0f7a78bae10830ca80416c90a7e30
Terseo <44824248+Terseo@users.noreply.github.com>
2022-06-29 10:42:31 -0400

    Exercise Running all tests and completing the scripts

---
+++ PageObjects/Unosquare/mainpage.js
@@ -0,0 +1,20 @@
+module.exports = {
+    url: 'https://www.unosquare.com',
+    elements: {
+      contactusMenu: {
+        selector: "li a[href = '/ContactUs']"
+      },
+      industriesMenu: {
+        selector: "li a[href = '/Industries']"
+      },
+      aboutMenu: {
+        selector: "li a[href = '/About']"
+      },
+      servicesMenu: {
+        selector: "li a[href = '/Services']"
+      },
+      contactForm: {
+        selector: "input[name='name']"
+      }
+    }
+  };
--- tests/unosquare/unosquare.js
+++ tests/unosquare/unosquare.js
@@ -1,22 +1,35 @@
 module.exports = {
-    'test unosquare contactUs' : function(browser) {
-      browser
-      .windowMaximize()
-      .url('https://www.unosquare.com')
-      .waitForElementVisible('body')
-      .click("li a[href = '/ContactUs']")
-      .click("li a[href = '/Industries']")
-      browser.end();
+    'Test1: Validate Contact Us have the correct Text': function (browser) {
+        var unosquare = browser.page.mainpage();
+
+        unosquare
+            .navigate()
+            .waitForElementVisible('@contactusMenu')
+            .assert.containsText('@contactusMenu', "CONTACT US")
+        browser.end();
     },
-  
-    'test unosquare V2' : function(browser) {
-      browser
-      .windowMaximize()
-      .url('https://www.unosquare.com')
-      .waitForElementVisible('body')
-      .click("li a[href = '/ContactUs']")
-      .click("li a[href = '/Industries']")
-      .assert.cssProperty("li a[href = '/About']", 'font-size', '12px') // This assert is going to fail
-      browser.end();
+
+    'Test2: Validate Industries url': function (browser) {
+        var unosquare = browser.page.mainpage();
+
+        unosquare
+            .navigate()
+            .waitForElementVisible('@industriesMenu')
+            .click('@industriesMenu')
+            .assert.urlEquals('https://www.unosquare.com/Industries')
+        browser.end();
+    },
+
+    'Test3: Validate that is possible set a value in contact us form': function (browser) {
+        var unosquare = browser.page.mainpage();
+
+        unosquare
+            .navigate()
+            .waitForElementVisible('@aboutMenu')
+            .click('@contactusMenu')
+            .setValue('@contactForm', 'Automation')
+            .assert.containsText('@contactForm', 'Test Fail')
+
+        browser.end();
     }
-  };
+}
