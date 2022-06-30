c3ffba43d2ac6d7b3d46dcd30d1b9b29678f8d5b
Terseo <44824248+Terseo@users.noreply.github.com>
2022-06-30 15:44:53 -0400

    Creating a script that with external data file

--- tests/unosquare/unosquare.js
+++ tests/unosquare/unosquare.js
@@ -1,18 +1,43 @@
 module.exports = {
-    'test command in blog page': function (browser) {
+
+    'test1: open unosquare': function (browser) {
         var unosquare = browser.page.mainpage();
         unosquare
             .navigate()
-            .contactUnosquare();
-
-        browser.end();
     },
-    'test command in services page': function (browser) {
+
+    'test2: open about page': function (browser) {
+        var unosquare = browser.page.mainpage();
+        unosquare
+            .openBlogPageUnosquare()
+    },
+
+    'test3: validate url and header name': function (browser) {
+        var unosquare = browser.page.mainpage();
+        unosquare
+            .validateUrlAndHeader()
+    },
+
+    'test4: validate recent and popular post are visible': function (browser) {
+        var unosquare = browser.page.mainpage();
+        unosquare
+            .validateRecentAndPopularPost()
+    },
+
+    'test5: blog page with external data': function (browser) {
+        var unosquare = browser.page.mainpage();
+        unosquare
+            .validateFilterNames("qa")
+            .validateFilterNames("development")
+            .validateFilterNames("java")
+            .validateFilterNames("testing")
+    },
+
+    'test6: about page with external data': function (browser) {
         var unosquare = browser.page.mainpage();
         unosquare
             .navigate()
-            .serviceUnosquare();
-
-        browser.end();
+            .validateAboutPageUnosquare()
     }
+    
 }; 
